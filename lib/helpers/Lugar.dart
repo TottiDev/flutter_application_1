2dab8b6c185b165eab56a1c323640d147578b402
TottiDev <emanuel.duque@upb.edu.co>
2022-08-23 13:31:44 -0500

    Restaurantes imagenes

--- lib/helpers/Lugar.dart
+++ lib/helpers/Lugar.dart
@@ -11,8 +11,7 @@ class Lugar extends StatefulWidget {
   State<Lugar> createState() => _LugarState();
 }
 
-class _LugarState extends State<Lugar>
-    with SingleTickerProviderStateMixin {
+class _LugarState extends State<Lugar> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   double _screenHeight = 0.0;
   double _screenWidth = 0.0;
@@ -39,20 +38,41 @@ class _LugarState extends State<Lugar>
       ),
       body: Stack(
         children: [
-          Bebidas(),
-          Comidas(),
-          Entradas(),
+          Restaurante1(),
+          Restaurante2(),
+          Restaurante3(),
+          Restaurante4(),
         ],
       ),
     );
   }
 
-  Widget Bebidas() {
+  Widget Restaurante1() {
     return Container(
-      alignment: const Alignment(0, 1),
+      alignment: const Alignment(-0.8, -0.8),
+      child: Container(
+        alignment: const Alignment(0, 0),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Image.network(
+              'https://images.unsplash.com/photo-1541086095944-f4b5412d3666?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.yellow,
+              onPrimary: Colors.deepOrangeAccent,
+              //padding: EdgeInsets.all(10.0),
+              maximumSize: Size(_screenHeight * 0.3, _screenWidth * 0.3)),
+        ),
+      ),
+    );
+  }
+
+  Widget Restaurante2() {
+    return Container(
+      alignment: const Alignment(0.8, -0.8),
       child: ElevatedButton(
         onPressed: () {},
-        child: Image.network('https://pbs.twimg.com/profile_images/1380267041790300166/uXdEuQ_D_400x400.png'),
+        child: Image.network(
+            'https://images.unsplash.com/photo-1606021511779-aae22948af3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
         style: ElevatedButton.styleFrom(
             primary: Colors.yellow,
             onPrimary: Colors.deepOrangeAccent,
@@ -62,32 +82,34 @@ class _LugarState extends State<Lugar>
     );
   }
 
-  Widget Comidas() {
+  Widget Restaurante3() {
     return Container(
-      alignment: const Alignment(0, 0),
+      alignment: const Alignment(-0.8, 0.8),
       child: ElevatedButton(
         onPressed: () {},
-        child: Text("Comidas"),
+        child: Image.network(
+            'https://images.unsplash.com/photo-1584536301769-0c5c357941db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
         style: ElevatedButton.styleFrom(
             primary: Colors.yellow,
             onPrimary: Colors.deepOrangeAccent,
             //padding: EdgeInsets.all(10.0),
-            minimumSize: Size(_screenHeight * 0.1, _screenWidth * 0.3)),
+            maximumSize: Size(_screenHeight * 0.3, _screenWidth * 0.3)),
       ),
     );
   }
 
-  Widget Entradas() {
+  Widget Restaurante4() {
     return Container(
-      alignment: const Alignment(0, -1),
+      alignment: const Alignment(0.8, 0.8),
       child: ElevatedButton(
         onPressed: () {},
-        child: Text("Entradas"),
+        child: Image.network(
+            'https://images.unsplash.com/photo-1589459985166-966c72e767d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
         style: ElevatedButton.styleFrom(
             primary: Colors.yellow,
             onPrimary: Colors.deepOrangeAccent,
             //padding: EdgeInsets.all(10.0),
-            minimumSize: Size(_screenHeight * 0.1, _screenWidth * 0.3)),
+            maximumSize: Size(_screenHeight * 0.3, _screenWidth * 0.3)),
       ),
     );
   }
